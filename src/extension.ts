6086b75dbbf7d51c614c59618e2dda6a308acf98
xackus <14938807+xackus@users.noreply.github.com>
2020-01-21 19:14:30 +0100

    use language server

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,13 @@
 'use strict';
 import * as vscode from 'vscode';
-import ZigCompilerProvider from './zigCompilerProvider';
+import { LanguageClientOptions, ServerOptions, LanguageClient } from 'vscode-languageclient';
 import { ZigFormatProvider, ZigRangeFormatProvider } from './zigFormat';
 
 const ZIG_MODE: vscode.DocumentFilter = { language: 'zig', scheme: 'file' };
 
-export function activate(context: vscode.ExtensionContext) {
-    let compiler = new ZigCompilerProvider();
-    compiler.activate(context.subscriptions);
-    vscode.languages.registerCodeActionsProvider('zig', compiler);
+let client: LanguageClient;
 
+export function activate(context: vscode.ExtensionContext) {
     const zigFormatStatusBar = vscode.window.createStatusBarItem(
         vscode.StatusBarAlignment.Left,
     );
@@ -28,7 +26,31 @@ export function activate(context: vscode.ExtensionContext) {
             new ZigRangeFormatProvider(logChannel),
         ),
     );
-}
 
-export function deactivate() {
+    const conf = vscode.workspace.getConfiguration("zig");
+	
+	let serverOptions: ServerOptions = {
+		command: conf.get<string>("zigPath") || 'zig',
+		args: ['run', '/home/mwa/code/zig/language-server/server.zig'],
+		options: { stdio: 'pipe' }
+	};
+
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'zig' }]
+	};
+
+	client = new LanguageClient(
+		'zigLanguageServer',
+		'Zig Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	client.start();
+}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
